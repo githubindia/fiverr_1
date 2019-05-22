450f4ea29853228555005ee8456f188f7f82a60e
Midhun m Nair <39356@hexaware.com>
2019-05-22 20:45:03 +0530

    chages regarding nav images and content refractoring

--- js/main.js
+++ js/main.js
@@ -15,6 +15,27 @@ jQuery(document).on('ready', function() {
 		});
 	}
 	collapseMenu();
+
+/*  Accordiant Open and Close */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+/* End of Accordian */
+
 	/*--------------------------------------
 			MEGA MENU						
 	--------------------------------------*/
